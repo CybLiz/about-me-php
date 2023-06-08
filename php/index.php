b87358b2a0730b07e6b7d9ae4c7a5ef7e7da8441
CybLiz <liza.bahloul@gmail.com>
2023-06-08 13:17:21 +0200

    pdf

--- php/index.php
+++ php/index.php
@@ -1,35 +1,42 @@
-
-
-
 <!DOCTYPE html>
 <html lang="en">
-<?php include './includes/head.html'; ?>
+
+<head>
+	<?php include './includes/head.html'; ?>
+
+	<link rel="stylesheet" href="css/aboutmeStyle.css">
+	<link rel="stylesheet" href="css/dreamsStyle.css">
+	<link rel="stylesheet" href="css/mypassionsStyle.css">
+	<link rel="stylesheet" href="css/erreur404.css">
+	<link rel="stylesheet" href="css/style.css">
+
+</head>
+
 
 <body>
-<?php include './includes/header.html'; ?>
-<?php include './includes/nav.html'; ?>
+	<?php include './includes/header.html'; ?>
+	<?php include './includes/nav.html'; ?>
 
-	
+
 	<?php
 
-if (isset($_GET ['pname'])){
-	$page = $_GET['pname'];
+	if (isset($_GET['pname'])) {
+		$page = $_GET['pname'];
 
-    if ($page ==='aboutme.html'){
-		include './includes/aboutme.html';
+		if ($page === 'aboutme.html') {
+			include './includes/aboutme.html';
+		} else if ($page === 'dreams.html') {
+			include './includes/dreams.html';
+		} else if ($page === 'passions.html') {
+			include './includes/passions.html';
+		} else {
+			include './includes/erreur404.html';
+		}
+	} else {
+		include './includes/home.html';
 	}
-	else if ($page === 'dreams.html'){
-		include './includes/dreams.html';
-	}
-	else if ($page ==='passions.html'){
-		include './includes/passions.html';
-	}
+	?>
 
-} else { 
-	include './includes/home.html';
-}
-    ?>
-	
 
 	<?php include './includes/footer.html'; ?>
 </body>
